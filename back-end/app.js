22f6e30b83db7584dbc7c00452dc62a1a7a5b4bd
JoseJunior23 <juniorferreira1919@hotmail.com>
2020-08-28 01:28:27 +0000

    (27/08) Configuração da conexão com o banco de dados

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 
+const db = require('./config/database');
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+
+
+db(`mongodb+srv://${dbUser}:${dbPass}@cluster0.rdarn.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`)
+
 var app = express();
 
 app.use(logger('dev'));
