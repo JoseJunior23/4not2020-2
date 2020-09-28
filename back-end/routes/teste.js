3cea2e92398281dd35733069df9f861e04174fbe
JoseJunior23 <juniorferreira1919@hotmail.com>
2020-09-28 22:59:53 +0000

    28/9 correção de erros

--- back-end/routes/teste.js
+++ back-end/routes/teste.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.send('Isto é um teste');
-=======
-  res.send('Isto é um teste COM NODEMON');
->>>>>>> upstream/master
 });
 
 module.exports = router;
