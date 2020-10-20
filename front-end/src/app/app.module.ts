65f6d5d3668d8d966c2711b9add6b817d8f0d3ba
JoseJunior23 <juniorferreira1919@hotmail.com>
2020-10-20 01:31:53 +0000

    19/10 iniciando o front-end  com angular

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './ui/principal/principal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    MainToolbarComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
